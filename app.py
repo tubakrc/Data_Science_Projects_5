34eb3705bb269f643487e88bb34844342e6e4815
tubakirca <118131917+tubakrc@users.noreply.github.com>
2025-06-13 11:47:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def main():
 
         # st.title("HOME")
         st.markdown("""
-            <h1 style="color:#c30452; padding-right:15px;">WEIGHT WISE</h1>
+            <h1 style="color:#c30452; padding-left:20px;">WEIGHT WISE</h1>
         """, unsafe_allow_html=True)
 
         # Resim dosyasını yükleme ve gösterme
